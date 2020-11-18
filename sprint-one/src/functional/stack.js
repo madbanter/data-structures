6b7a1a807940eac43351ca3e8b81b263e40238ed
Adam Brent <adam.brent@gmail.com>
2020-11-18 11:38:56 -0800

    Adds completed stack (functional section).

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,25 @@
 var Stack = function() {
   var someInstance = {};
+  var itemCount = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    itemCount++;
+    storage[itemCount] = value;
   };
 
   someInstance.pop = function() {
+    var popped = storage[itemCount];
+    delete storage[itemCount];
+    itemCount--;
+    return popped;
   };
 
   someInstance.size = function() {
+    return Math.max(itemCount, 0);
   };
 
   return someInstance;
