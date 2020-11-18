1e98fabfc89911cdf5540ec7a35e5bc786aef8f0
Adam Brent <adam.brent@gmail.com>
2020-11-18 12:57:56 -0800

    Adds completed queue (functional section).

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,5 +1,8 @@
 var Queue = function() {
   var someInstance = {};
+  var itemCount = 0;
+  var newestKey = 0;
+  var oldestKey = 1;
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -7,12 +10,28 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    itemCount++;
+    newestKey++;
+    storage[newestKey] = value;
   };
 
   someInstance.dequeue = function() {
+    if (itemCount > 0) {
+      itemCount--;
+      var dequeued = storage[oldestKey];
+      delete storage[oldestKey];
+      if (itemCount > 0) {
+        oldestKey++;
+      } else {
+        oldestKey = 1;
+        newestKey = 0;
+      }
+      return dequeued;
+    }
   };
 
   someInstance.size = function() {
+    return itemCount;
   };
 
   return someInstance;
