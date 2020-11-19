e0fbd437d9b5974bb7135a3d65d48674c44beae6
Adam Brent <adam.brent@gmail.com>
2020-11-18 16:03:12 -0800

    Adds completed stack (prototypal section).

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,26 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var stackInstance = Object.create(stackMethods);
+  stackInstance.itemCount = 0;
+  stackInstance.storage = {};
+  return stackInstance;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(item) {
+  this.itemCount++;
+  this.storage[this.itemCount] = item;
+};
 
+stackMethods.pop = function() {
+  if (this.itemCount > 0) {
+    var popped = this.storage[this.itemCount];
+    delete this.storage[this.itemCount];
+    this.itemCount--;
+    return popped;
+  }
+};
+
+stackMethods.size = function() {
+  return this.itemCount;
+};
