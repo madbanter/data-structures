a91c81093bd00e496296b1d9e8d52a6f514609de
Adam Brent <adam.brent@gmail.com>
2020-11-18 20:22:05 -0800

    Adds completed queue (ES6 section).

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,31 @@
 class Queue {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.itemCount = 0;
+    this.newestKey = 0;
+    this.oldestKey = 1;
   }
 
+  enqueue(value) {
+    this.itemCount++;
+    this.newestKey++;
+    this.storage[this.newestKey] = value;
+  }
+
+  dequeue() {
+    if (this.itemCount > 0) {
+      let dequeued = this.storage[this.oldestKey];
+      delete this.storage[this.oldestKey];
+      this.oldestKey++;
+      if (this.itemCount > 0) {
+        this.itemCount--;
+      }
+      return dequeued;
+    }
+
+  }
+
+  size() {
+    return this.itemCount;
+  }
 }
