baedb1a9ac70e8e75c2903fc7028ede42e35d683
Adam Brent <adam.brent@gmail.com>
2020-11-18 16:24:06 -0800

    Adds completed queue (pseudoclassical section).

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,31 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.itemCount = 0;
+  this.newestKey = 0;
+  this.oldestKey = 1;
 };
 
+Queue.prototype.enqueue = function(value) {
+  this.itemCount++;
+  this.newestKey++;
+  this.storage[this.newestKey] = value;
+};
 
+Queue.prototype.dequeue = function() {
+  if (this.itemCount > 0) {
+    this.itemCount--;
+    var dequeued = this.storage[this.oldestKey];
+    delete this.storage[this.oldestKey];
+    if (this.itemCount > 0) {
+      this.oldestKey++;
+    } else {
+      this.oldestKey = 1;
+      this.newestKey = 0;
+    }
+    return dequeued;
+  }
+};
+
+Queue.prototype.size = function() {
+  return this.itemCount;
+};
